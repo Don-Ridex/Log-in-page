345606c60169a3efdb7d68e07260e079122152c2
Ulephraim <iphraimunlimited@gmail.com>
2024-11-04 22:29:47 +0100

    created react app for login page

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import './index.css';
+import Login from './Screens/loginScreen';
+
+function App() {
+  return <Login />;
+}
+
+export default App;
---
+++ client/src/Screens/loginScreen.js
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from 'react';
+import './loginScreen.css';
+
+const Login = () => {
+  return <h1>This is a Login Page</h1>;
+};
+export default Login;
